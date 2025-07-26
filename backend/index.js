81e0aee559158684881e5636c9c5b46917124a29
Aman-vijay <shivamvj04@gmail.com>
2025-07-26 17:26:30 +0530

    Add frontend components for Live Bookings Dashboard
    - Created index.html for the main dashboard interface with real-time booking updates.
    - Added styles.css for styling the dashboard, including layout and animations.
    - Included a screenshot image for visual representation.

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,19 @@ const app = express();
 const http = require("http")
 const path = require("path")
 const {Server} = require("socket.io")
+const cors = require("cors")
 
 
 
 const server = http.createServer(app)
-const io = new Server(server)
-app.use(express.static(path.resolve("./public")));
+const io = new Server(server,{
+  cors:{
+    origin:"http:localhost:3000",
+    methods:["GET", "POST"]
+  }
+
+})
+// app.use(express.static(path.resolve("./public")));
 
 //Array for storing
 let bookings = [];
@@ -36,7 +43,7 @@ setInterval(()=>{
 },5000)
 
 function generateRandomBooking() {
-  const venues = ['Cafe Mocha', 'Green Garden', 'Ocean View', 'The Hangout', 'Skyline Lounge'];
+  const venues = ['Cafe Mocha', 'Green Garden', 'Ocean View', 'The Hangout', 'Skyline Lounge','Hotel Lagoon',"The Leela"];
   const randomVenue = venues[Math.floor(Math.random() * venues.length)];
   const partySize = Math.floor(Math.random() * 6) + 1; 
   const time = new Date().toLocaleTimeString();
